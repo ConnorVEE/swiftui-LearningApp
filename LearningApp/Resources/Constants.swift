ccf365d30e772e3f268becdf74fa39c574e48e59
ConnorVEE <61746584+ConnorVEE@users.noreply.github.com>
2021-08-27 15:59:47 -0500

    - This is so late in the project and so much has changed

---
+++ LearningApp/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  LearningApp
+//
+//  Created by Connor VanTress on 8/20/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videoHostUrl = "https://codewithchris.github.io/learningJSON/"
+    
+}
